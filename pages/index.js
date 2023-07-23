092e973033fb0c19135f28e25e19c073aac498f2
Bryan W <bhoff1980@gmail.com>
2023-07-22 21:13:44 -0600

    updated image listings from locofy import

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ const LandingPage = () => {
         <title>Real Estate</title>
         <meta name="description" content="Search for home listings" />
       </Head>
-      <div className="relative bg-gray-white w-full h-[5095.99px] flex flex-col items-center justify-start">
+      <div className="relative bg-gray-white w-full  flex flex-col items-center justify-start">
         <Header hamburger={false} />
         <Hero />
         <AreaContainer />
@@ -23,15 +23,7 @@ const LandingPage = () => {
         <RentPropertiesContainer />
         <RentPropertiesForm />
         <Contact />
-        <Footer
-          houseLine="/houseline2.svg"
-          socialMediaLogo="/social-media-logo5.svg"
-          socialMediaLogo1="/social-media-logo6.svg"
-          socialMediaLogo2="/social-media-logo7.svg"
-          socialMediaLogo3="/social-media-logo8.svg"
-          socialMediaLogo4="/social-media-logo9.svg"
-          propOverflow="unset"
-        />
+        <Footer/>
       </div>
     </>
   );
